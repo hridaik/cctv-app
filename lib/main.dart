1bc1d956cfc2eaa0e38698a0a465eaeb4b41a500
hridaik <hridaik007@gmail.com>
2022-09-29 02:34:48 +0530

    checknow

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,6 +7,62 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CCTV',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  String location = 'Null, Press Button';
+  String Address = 'search';
+
+  Future<Position> _getGeoLocationPosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      await Geolocator.openLocationSettings();
+      return Future.error('Location services are disabled.');
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,18 +70,23 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: ElevatedButton(
-            child: Text('+j'),
-            onPressed: () {
-              
-            },
-            style: ElevatedButton.styleFrom(
-              fixedSize: const Size(150, 150),
-              shape: const CircleBorder(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text(location, style: TextStyle(color: Colors.black, fontSize: 16)),
+            ElevatedButton(
+                onPressed: () async {
+                  Position position =
+                      await _getGeoLocationPosition(); // position will have the data
 
-              )
-            ),
-          ),
+                  location =
+                      'Lat: ${position.latitude}, Long: ${position.longitude}';
+
+                  setState(() {});
+                },
+                style: ElevatedButton.styleFrom(
+                    fixedSize: const Size(150, 150),
+                    shape: const CircleBorder()),
+                child: Text('+j'))
+          ]),
         ),
         appBar: AppBar(
           title: const Text('Heloo'),
@@ -33,4 +95,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
